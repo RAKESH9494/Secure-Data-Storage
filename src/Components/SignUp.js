6dac3457f256e2c4105e4d22ed7d07c132d334d1
RAKESH UMMADI <104605032+RAKESH9494@users.noreply.github.com>
2023-07-08 00:57:29 +0530

    Update SignUp.js

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -1,27 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import "../App.css"
 import { connect } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { ethers } from 'ethers'
 const SignUp = ({account,contract}) =>{
     const navigate = useNavigate();
+    const [status,setStatus] = useState();
     useEffect(()=>{
         document.querySelector("#Address").value = account;
     },[account])
     const submitHandler = async(e) =>{
         e.preventDefault();
+        setStatus("Please wait...")
         const Fname = document.querySelector("#Fname").value;
         const Lname = document.querySelector("#Lname").value;
         const Gmail = document.querySelector("#Gmail").value;
         const password = document.querySelector("#password").value;
         try{
-            const amount = {value : ethers.utils.parseEther("1")} 
+            const amount = {value : ethers.utils.parseEther("0.00001")} 
             const transaction = await contract.Signup(Fname,Lname,password,Gmail,account,amount)
             await transaction.wait();
+            setStatus("")
             alert("Submited")
             navigate("/")
         }catch(e){
-            alert(e);
+            alert(e.reason);
+            setStatus("")
         }
     }
     const handler1  = async(e) =>{
@@ -39,10 +43,10 @@ return (
     <div>
         <center>
         <p>Connected : {account}</p>
-        <button onClick={handler1}>CHANGE</button>
+        <button onClick={handler1} class="btn btn-primary">CHANGE</button>
             <div class="signUpbox">
               <h2>SIGN UP</h2>
-              <form class="form-group" onSubmit={submitHandler}>
+              <form class="form-group">
                 <label>FIRST NAME</label>
                 <input type="text" class="form-control" id="Fname"/>
                 <label>LAST NAME</label>
@@ -53,7 +57,8 @@ return (
                 <input type="text" class="form-control" id="Gmail"/>
                 <label>PASSWORD</label>
                 <input type="text" class="form-control" id="password"/><br/>
-                <input type="submit" value={"SUBMIT"} class="btn btn-secondary"/>
+                <input type="submit" onClick={submitHandler}value={"SUBMIT"} class="btn btn-secondary"/>&nbsp;
+                <button class="btn btn-secondary" onClick={()=>navigate("/")}>CLOSE</button>&nbsp;<span>{status}</span>
                </form>
            </div>
         </center>
